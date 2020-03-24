f7a93c7e8e4477ce29572bd75c6997c43ba4bab9
uuso <mymail.uuso@gmail.com>
2020-03-24 22:04:37 +0300

    task complete; waiting for heroku deployment

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-
+import sentry_key
 from bottle import Bottle, run
 import sentry_sdk
 from sentry_sdk.integrations.bottle import BottleIntegration
 
 sentry_sdk.init(
-    dsn="https://b0747e52ceec422280144fa1100e6372@sentry.io/5173623",
+    dsn=sentry_key.dsn,
     integrations=[BottleIntegration()]
 )
 
@@ -13,7 +13,14 @@ app = Bottle()
 @app.route('/')
 def index():
     raise ValueError("It's a test message only!")
-    # return
+
+@app.route('/success')
+def success():
+    return "It's OK!"
+    
+@app.route('/fail')
+def fail():
+    raise RuntimeError("Failure!")
 
 
 app.run(host = "10.186.0.248", port=8080)
